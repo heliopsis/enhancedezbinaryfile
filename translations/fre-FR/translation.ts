5e8ec1001cf4269a26fc8fb56031fdadcb9f2f66
Benjamin Choquet <benjamin.choquet@heliopsis.net>
2012-10-25 14:31:09 +0200

    french translation

---
+++ translations/fre-FR/translation.ts
@@ -0,0 +1,63 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+	<context>
+		<name>design/standard/class/datatype</name>
+		<message>
+			<source>Max. file size</source>
+			<translation>Poids max. du fichier</translation>
+		</message>
+	</context>
+	<context>
+		<name>design/standard/content/datatype</name>
+		<message>
+			<source>succesfully uploaded.</source>
+			<translation>envoyé avec succès.</translation>
+		</message>
+		<message>
+			<source>Link</source>
+			<translation>Lien</translation>
+		</message>
+		<message>
+			<source>No file uploaded.</source>
+			<translation>Aucun fichier envoyé.</translation>
+		</message>
+	</context>
+	<context>
+		<name>design/standard/user</name>
+		<message>
+			<source>Fields marked with an asterisk</source>
+			<translation>Les champs marqués d'une astérisque</translation>
+		</message>
+		<message>
+			<source>are required.</source>
+			<translation>sont obligatoires</translation>
+		</message>
+	</context>
+	<context>
+		<name>design/base</name>
+		<message>
+			<source>Send</source>
+			<translation>Envoyer</translation>
+		</message>
+	</context>
+	<context>
+		<name>extension/enhancedezbinaryfile/datatype</name>
+		<message>
+			<source>Enhanced File</source>
+			<translation>Fichier amélioré</translation>
+		</message>
+	</context>
+	<context>
+		<name>kernel/classes/datatypes</name>
+		<message>
+			<source>Enhanced File</source>
+			<translation>Fichier amélioré</translation>
+		</message>
+        <message>
+            <source>Failed to store file. Only the following file types are allowed: %1.</source>
+            <translation>Impossible d'enregistrer le fichier. Seuls les types suivants sont autorisés : %1</translation>
+        </message>
+	</context>
+</TS>
+
---
+++ translations/untranslated/translation.ts
@@ -0,0 +1,63 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+	<context>
+		<name>design/standard/class/datatype</name>
+		<message>
+			<source>Max. file size</source>
+			<translation type="unfinished" />
+		</message>
+	</context>
+	<context>
+		<name>design/standard/content/datatype</name>
+		<message>
+			<source>succesfully uploaded.</source>
+			<translation type="unfinished" />
+		</message>
+		<message>
+			<source>Link</source>
+			<translation type="unfinished" />
+		</message>
+		<message>
+			<source>No file uploaded.</source>
+			<translation type="unfinished" />
+		</message>
+	</context>
+	<context>
+		<name>design/standard/user</name>
+		<message>
+			<source>Fields marked with an asterisk</source>
+			<translation type="unfinished" />
+		</message>
+		<message>
+			<source>are required.</source>
+			<translation type="unfinished" />
+		</message>
+	</context>
+	<context>
+		<name>design/base</name>
+		<message>
+			<source>Send</source>
+			<translation type="unfinished" />
+		</message>
+	</context>
+	<context>
+		<name>extension/enhancedezbinaryfile/datatype</name>
+		<message>
+			<source>Enhanced File</source>
+			<translation type="unfinished" />
+		</message>
+	</context>
+	<context>
+		<name>kernel/classes/datatypes</name>
+		<message>
+			<source>Enhanced File</source>
+			<translation type="unfinished" />
+		</message>
+        <message>
+            <source>Failed to store file. Only the following file types are allowed: %1.</source>
+            <translation type="unfinished" />
+        </message>
+	</context>
+</TS>
+
